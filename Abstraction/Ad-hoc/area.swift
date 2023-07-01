cc3c01e002f6d677ee5d642ba7ff93c3efba46a4
AE-0 <64426945+AE-0@users.noreply.github.com>
2023-07-01 12:40:38 +0000

    Create area.swift

---
+++ Abstraction/Ad-hoc/area.swift
@@ -0,0 +1,10 @@
+func printArea(_ side: Int) {
+    print("Area of square: \(side * side)")
+}
+
+func printArea(_ height: Int, _ width: Int) {
+    print("Area of rectangle: \(height * width)")
+}
+
+printArea(5)
+printArea(4, 6)
